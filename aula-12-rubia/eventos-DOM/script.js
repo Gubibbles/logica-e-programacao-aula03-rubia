d2d8fbb14d7fa838be51c1e31264e6f93c8f70f6
Gubibbles <mrsgbbs@hotmail.com>
2025-09-19 10:37:13 -0300

    aula-12-rubia

---
+++ aula-12-rubia/eventos-DOM/script.js
@@ -0,0 +1,15 @@
+
+function entradaMouse() {
+    let caixa = document.getElementById("mouse")
+    caixa.style.backgroundColor = "pink"
+}
+
+function saidaMouse() {
+    let caixa = document.getElementById("mouse")
+    caixa.style.backgroundColor = "lightblue"
+}
+
+function mudaHtml() {
+    let nota = document.querySelector("nome").value
+    caixa.inenrHTML = nome
+}
